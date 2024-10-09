b4e63547a242a6fc81de0da8fe2ef926aac05ac8
qlieuash <qlieuash@students.cs.ubc.ca>
2024-10-08 17:25:22 -0700

    Made Habit Class abstract, and added a field

--- src/main/model/Habit.java
+++ src/main/model/Habit.java
@@ -1,26 +1,32 @@
 package model;
 
-// Represents a Habit, with a title, a long term goal (# of times recorded), a number of shortterm goals (intermediate milestones), 
-//a list of short term goals, progress (# times habit is completed), and a reward message.
-public class Habit {
-    private String title;
-    private int longGoal;
-    private int shortGoal;
-    private ArrayList<shortGoal> goals;
-    private int progress;
-    private String reward;
+// Represents a Habit, with a title, a long term goal (# of times recorded), a short-term goals (intermediate milestones), 
+// progress (# times habit is completed), a remaining progress (habit recordings left til long goal is accomplished) 
+// and a reward message.
+public abstract class Habit {
+    protected String title;
+    protected int longGoal;
+    protected int shortGoal;
+    protected int progress;
+    protected int remaining;
+    protected String reward;
 
     public Habit(String habitName, int target, int intermediates, String rewardMessage) {
         title = habitName;
         longGoal = target;
         shortGoal = longGoal / intermediates; 
         progress = 0;
+        remaining = longGoal - progress;
         reward = rewardMessage;
     }
 
     //MODIFIES: This
-    //EFFECTS: increases the progress of habit by 1
-    public void recordHabit() {
+    //EFFECTS: increases the progress of habit
+    public abstract void recordHabit();
+
+    //MODIFIES: This
+    //EFFECTS: resets the habit progress to 0
+    public void resetProgress() {
         //stub
     }
 
