b01a540d2ad717bb455df3a5c3548ff5dea67fed
qlieuash <qlieuash@students.cs.ubc.ca>
2024-10-10 19:31:47 -0700

    Implemented methods in HabitTracker

--- src/main/model/HabitTracker.java
+++ src/main/model/HabitTracker.java
@@ -19,40 +19,45 @@ public class HabitTracker {
 
     //MODIFIES: this
     //EFFECTS: adds a habit to habits, and adds it to todo
-    public void addHabit() {
-        //stub
+    public void addHabit(Habit h) {
+        habits.add(h);
+        todo.add(h);
     }
 
     //MODIFIES: this
     //EFFECTS: removes habit form todo, and adds habit to completed
-    public void markCompleted() {
-        //stub
+    public void markCompleted(Habit h) {
+        todo.remove(h);
+        completed.add(h);
     }
 
     //MODIFIES: this 
     //EFFECTS: removes habit from completed, and adds habit to todo.
-    public void markTodo() {
-        //stub
+    public void markTodo(Habit h) {
+        completed.remove(h);
+        todo.add(h);
     }
     
     //MODIFIRES: this
     //EFFECTS: remove a habit from all lists
-    public void removeHabit() {
-        //stub
+    public void removeHabit(Habit h) {
+        habits.remove(h);
+        completed.remove(h);
+        todo.remove(h);
     }
 
     public ArrayList<Habit> getAllHabits() {
-        return new ArrayList<>();//stub
+        return new ArrayList<>(habits);
     }
 
     public ArrayList<Habit> getCompleted() {
-        return new ArrayList<>();//stub
+        return new ArrayList<>(completed);
     }
 
     public ArrayList<Habit> getTodo() {
-        return new ArrayList<>();//stub
+        return new ArrayList<>(todo);
     }
 
-    
+
 
 }
