815b98eea67e7a21d2cab92a76f547d6af0790de
qlieuash <qlieuash@students.cs.ubc.ca>
2024-10-11 13:40:21 -0700

    Added implementation for main menu and added new methods stubs based on main menu options

--- src/main/ui/HabitTrackerApp.java
+++ src/main/ui/HabitTrackerApp.java
@@ -1,28 +1,83 @@
 package ui;
 
+import model.HabitTracker;
+
+import java.util.Scanner;
+
 //Habit Tracker Application
 public class HabitTrackerApp {
+    private Scanner input;
+    private HabitTracker habits;
+    private int habitIndex;
+    private boolean keeprunning = true;
 
     //EFFECTS: Runs HabitTracker App
     public HabitTrackerApp() {
-        //stub
+        runHabitTracker();
     }
 
     //MODIFIES: this
     //EFFECTS: processes user input
     public void runHabitTracker() {
-        //stub
+        int command = 0;
+
+        init();
+        while (keeprunning) {
+        startScreen();
+        command = input.nextInt();
+        processMenuCommand(command);
+        }
+        
+        
     }
 
     //MODIFIES: this
     //EFFECTS: processes user commands for start screen
-    public void processMenuCommand() {
-        //stub
+    public void processMenuCommand(int decision) {
+        switch (decision) {
+            case 1:
+                displayAllHabits();
+                break;
+            case 2:
+                displayTodo();
+                break;
+            case 3: 
+                displayCompleted();
+                break;
+            case 4:
+                newHabit();
+                break;
+            case 5:
+                displayOverallProgress();
+                break;
+            case 6:
+                newDay();
+                break;
+            case 7:
+                quitApp();
+                break;    
+            default:
+                System.out.println("Invalid input. Please selected again.");
+        }
     }
 
     //EFFECTS: shows all options available to user
     public void startScreen() {
-        //stub
+        System.out.println("\nWelcome to you personal habit tracker!\\n" + "Please select one of the options below\n");
+        System.out.println("\n\t1. View habits");
+        System.out.println("\t2. View TODO habits");
+        System.out.println("\t3. View completed habits");
+        System.out.println("\t4. Create new habit");
+        System.out.println("\t5. Get progress report");
+        System.out.println("\t6. New day (testing purposes)");
+        System.out.println("\t7. Quit");
+    }
+
+    //EFFECTS: intitializes the HabitTracker
+    public void init() {
+        habits = new HabitTracker();
+        input = new Scanner(System.in);
+        input.useDelimiter("\r?\n|\r");
     }
 
     //EFFECTS: displays the options available for one habit 
@@ -54,7 +109,7 @@ public class HabitTrackerApp {
         //stub
     }
 
-    //EFFECTS: displays list of all habits
+    //EFFECTS: displays list of all habits, one habit at a time
     public void displayAllHabits() {
         //stub
     }
@@ -82,5 +137,16 @@ public class HabitTrackerApp {
         //stub
     }
 
+    //MODIFIES: this
+    //EFFECTS: advances day for testing purposes
+    public void newDay() {
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: turns keepRunning field to false, and prints final message
+    public void quitApp() {
+        //stub
+    }
     
 }
