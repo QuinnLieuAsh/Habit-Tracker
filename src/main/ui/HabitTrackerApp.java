42df3d8845146b2cec8dc6998826ad611f21990e
qlieuash <qlieuash@students.cs.ubc.ca>
2024-10-11 10:54:07 -0700

    Created HabitTrackerApp class with documentation and method stubs

---
+++ src/main/ui/HabitTrackerApp.java
@@ -0,0 +1,86 @@
+package ui;
+
+//Habit Tracker Application
+public class HabitTrackerApp {
+
+    //EFFECTS: Runs HabitTracker App
+    public HabitTrackerApp() {
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes user input
+    public void runHabitTracker() {
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes user commands for start screen
+    public void processMenuCommand() {
+        //stub
+    }
+
+    //EFFECTS: shows all options available to user
+    public void startScreen() {
+        //stub
+    }
+
+    //EFFECTS: displays the options available for one habit 
+    public void habitOptions() {
+        //stub
+        //TODO: Options: 1. Record Habit 2. Edit Habit       
+    }
+
+    //MODIFIES: this
+    //EFFECTS: process users commands for habit options.
+    public void processHabitCommand() {
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: displays a habits editable attributes
+    public void showEditAttributes() {
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: user edits a habits attributes
+    public void processEditCommands() {
+        //stub
+    }
+
+    //EFFECTS: shows a players overall progress on their habits.
+    public void displayOverallProgress() {
+        //stub
+    }
+
+    //EFFECTS: displays list of all habits
+    public void displayAllHabits() {
+        //stub
+    }
+
+    //EFFECTS: displays list of completed habits
+    public void displayCompleted() {
+        //stub
+    }
+
+
+    //EFFECTS: displays list of todo habits
+    public void displayTodo() {
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: Records a users completion of a habit
+    public void recordHabit() {
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: creates a new habit based on user specifications
+    public void newHabit() {
+        //stub
+    }
+
+    
+}
