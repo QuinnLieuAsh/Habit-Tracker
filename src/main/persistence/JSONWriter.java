ea3d4bea2b64e4e15fde73c1f4a85b8f7380f45f
qlieuash <qlieuash@students.cs.ubc.ca>
2024-10-24 21:26:31 -0700

    implemeneted JSONWriter

--- src/main/persistence/JSONWriter.java
+++ src/main/persistence/JSONWriter.java
@@ -17,31 +17,32 @@ public class JSONWriter {
 
     // EFFECTS: constructs writer to write to destination file
     public JSONWriter(String destination) {
-        //stub
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-        //stub
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of HabitTracker to file
     public void write(HabitTracker ht) {
-        //stub
+        JSONObject json = ht.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        //stub
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        //stub
+        writer.print(json);
     }
 }
