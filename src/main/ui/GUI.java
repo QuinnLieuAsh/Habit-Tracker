f0ee1d58e606876f91fd2d4ead4fc9de2be40928
qlieuash <qlieuash@students.cs.ubc.ca>
2024-11-19 22:00:47 -0800

    Created GUI class with documentation

---
+++ src/main/ui/GUI.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.DefaultListModel;
+
+// A GUI for the HabitTracker
+public class GUI {
+    private Controller controller;
+    private DefaultListModel<String> habitsModel;
+
+    public GUI(Controller controller) {
+        //stub
+    }
+
+}
