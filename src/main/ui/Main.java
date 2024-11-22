8ccd57231289bb98d4958c4cdab2264ffc924a65
qlieuash <qlieuash@students.cs.ubc.ca>
2024-11-21 23:33:43 -0800

    Added implementation in Main to run GUI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,10 +2,16 @@ package ui;
 
 import java.io.FileNotFoundException;
 
+import model.HabitTracker;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to my project!");
         try {
+            HabitTracker tracker = new HabitTracker();
+            Controller controller = new Controller(tracker);
+
+            new GUI(controller);
             new HabitTrackerApp();
         } catch (FileNotFoundException e) {
             System.out.println("Unable to run application: file not found");
